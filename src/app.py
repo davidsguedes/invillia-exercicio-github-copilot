0207a42f1eee10c2dfa4dc4f9f1680f8537b9518
David Guedes <dvdguedes@gmail.com>
2025-05-07 18:44:04 +0000

    Melhora a validação de inscrição para atividades extracurriculares

--- src/app.py
+++ src/app.py
@@ -98,9 +98,9 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specificy activity
     activity = activities[activity_name]
 
-    # Check if the email is already signed up
+    # Validar se o aluno já está inscrito
     if email in activity["participants"]:
-        raise HTTPException(status_code=400, detail="Email already signed up for this activity")
+        raise HTTPException(status_code=400, detail="Already signed up for this activity")
 
     # Add student
     activity["participants"].append(email)
